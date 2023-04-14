ebdc2889d2e1946f8c72574865ab35a1d9b0cab1
Jack P <jackpeterson99528@gmail.com>
2023-04-14 15:54:42 -0400

    added reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 var entry = document.getElementById("entry");
 var saveButton = document.getElementById("save");
+
+var clear = document.getElementById("reset");
+
 console.log(saveButton)
+console.log(clear)
+
+clear.addEventListener("click", function(event){
+console.log(clear)
+event.preventDefault();
+localStorage.clear();
+
+entry.value = "";
+
+
+});
+
+
 saveButton.addEventListener("click", function(event) {
     event.preventDefault();
 
